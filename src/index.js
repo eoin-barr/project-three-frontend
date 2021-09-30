0a0596ce1825a6fb8b0921268a90eddfad92fd59
eoin-barr <eoin.barr@ucdconnect.ie>
2021-09-30 14:04:37 +0100

    Mapbox issue fixed

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom'
 import './styles/main.scss'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
